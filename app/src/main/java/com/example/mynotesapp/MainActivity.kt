493c2162cfd753da373174ef1988091187e75c5a
raees <mohitdamke1@gmail.com>
2024-06-15 21:42:08 +0530

    a

--- app/src/main/java/com/example/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/example/mynotesapp/MainActivity.kt
@@ -10,32 +10,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.mynotesapp.data.Note
+import com.example.mynotesapp.presentation.NoteViewModel
 import com.example.mynotesapp.ui.theme.MyNotesAppTheme
-import com.example.mynotesapp.viewmodel.NoteViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -47,62 +40,67 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyNotesAppTheme {
 
+                var title by remember {
+                    mutableStateOf("")
+                }
+                var content by remember {
+                    mutableStateOf("")
+                }
+                var favColor by remember {
+                    mutableStateOf("")
+                }
+
+                val note = Note(
+                    title = title,
+                    content = content,
+                    favColor = favColor
+                )
+
+                var noteList by remember {
+                    mutableStateOf(listOf<Note>())
+                }
+
+                viewModel.getAllNotes().observe(this) {
+                    noteList = it
+                }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    var title by remember {
-                        mutableStateOf("")
-                    }
-                    var desp by remember {
-                        mutableStateOf("")
-                    }
-                    val note = Note(
-                        title = title,
-                        desp = desp
-                    )
-
-                    var noteList by remember {
-                        mutableStateOf(listOf<Note>())
-                    }
-
-                    viewModel.getAllNotes().observe(this) {
-                        noteList = it
-
-                    }
                     Column(
                         modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
                     ) {
                         OutlinedTextField(value = title, onValueChange = { title = it })
-                        Spacer(modifier = Modifier.height(20.dp))
-                        OutlinedTextField(value = desp, onValueChange = { desp = it })
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        Button(onClick = { viewModel.getUpsertNote(note) }) {
-                            Text(text = "Click", fontSize = 24.sp)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        OutlinedTextField(value = content, onValueChange = { content = it })
+                        Spacer(modifier = Modifier.height(16.dp))
+                        OutlinedTextField(value = favColor, onValueChange = { favColor = it })
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = { viewModel.getUpsert(note) }) {
+                            Spacer(modifier = Modifier.height(26.dp))
+                            Text(text = "Click")
                         }
 
-                        LazyColumn() {
+                        LazyColumn {
                             items(noteList) { index ->
-                                Card(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .clickable { viewModel.getDeleteNote(index) }
-                                        .padding(16.dp),
-                                    elevation = CardDefaults.cardElevation(20.dp)
-                                ) {
+                                Column(modifier = Modifier
+                                    .fillMaxSize()
+                                    .clickable { viewModel.getDelete(index) })
+                                {
                                     Text(text = index.title)
-                                    Text(text = index.desp)
+                                    Text(text = index.content)
                                 }
                             }
                         }
 
 
                     }
+
 
                 }
             }
